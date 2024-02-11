509dd39cde1812621d5b57083678c16b7d084953
JacekCzupyt <kuba.czupyt@gmail.com>
2024-02-11 22:15:15 +0100

    Remove lasers when off screen

---
+++ resources/scripts/Laser.cs
@@ -0,0 +1,12 @@
+using Godot;
+using MonoCustomResourceRegistry;
+
+namespace MovementTest.resources.scripts;
+
+[RegisteredType("Laser", "", nameof(Node2D))]
+public partial class Laser : Node2D {
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready() {
+		GetNode<VisibleOnScreenNotifier2D>("VisibleOnScreenNotifier2D").ScreenExited += QueueFree;
+	}
+}
