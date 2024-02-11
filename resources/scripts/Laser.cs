6431dfc707e6fdbf7f4794c0d1f8dd8287578ed1
JacekCzupyt <kuba.czupyt@gmail.com>
2024-02-11 22:34:44 +0100

    Scale lasers

--- resources/scripts/Laser.cs
+++ resources/scripts/Laser.cs
@@ -8,5 +8,6 @@ public partial class Laser : Node2D {
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready() {
 		GetNode<VisibleOnScreenNotifier2D>("VisibleOnScreenNotifier2D").ScreenExited += QueueFree;
+		GetNode<ScaleOnImpact>("ScaleOnImpact").TweenScale();
 	}
 }
